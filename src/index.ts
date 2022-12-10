07a8bc3c5221dc6297d02fa836ea06c5ceeda61d
nauxscript <nauxscript@gmail.com>
2022-12-10 10:32:41 +0800

    feat: add basic test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { Selection } from "./types"
+import type { Selection } from './types'
 
 export default function run(argv: Array<string | boolean >) {
   const query = argv[0]
-  const result:{
+  const result: {
     items: Selection[]
   } = {
     items: [{
       title: `This is a selection example, and you are inputing ${query}`,
-    }]
+    }],
   }
+
+  // for testing
+  if (argv[1])
+    return result
+
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(result, null, ''))
 }
